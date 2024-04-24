cd75cdc977403a512ed86d92ca5e051c4c378846
Mariana Mena <maranashokry@ahoo.com>
2024-04-24 17:47:50 -0500

    added proxy

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
+import ClipLoader from "./Spinner";
 
 function JobListings({ isHome = false }) {
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    //const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+
+    useEffect(() => {
+        //look at vite.config.js
+        const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+        const fetchJobs = async () => {
+            try {
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+                setJobs(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching jobs:", error);
+                setLoading(false);
+            }
+        };
+        fetchJobs();
+    }, []);
 
     //console.log(jobs);
     return (
@@ -12,12 +32,18 @@ function JobListings({ isHome = false }) {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? "Recent Jobs" : "Browse Jobs"}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobListings.map((job) => (
-                        <JobListing job={job} key={job.id} />
-                    ))}
-                    {/* <!-- Job Listing 1 --> */}
-                </div>
+                {/* {jobListings.map((job) => "Note: 3 jobs will appear"}( */}
+                {loading ? (
+                    <ClipLoader icon={true} loading={loading} />
+                ) : (
+                    <>
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {jobs.map((job) => (
+                                <JobListing job={job} key={job.id} />
+                            ))}
+                        </div>
+                    </>
+                )}
             </div>
         </section>
     );
