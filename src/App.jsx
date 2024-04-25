0663caa03774ef7eb18bc33f69b1d83d97e35421
Mariana Mena <maranashokry@ahoo.com>
2024-04-25 11:17:20 -0500

    update and delete

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,9 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
+//add job
 const addJob = async (newJob) => {
     //console.log(newJob);
     const res = await fetch(`/api/jobs`, {
@@ -30,6 +32,7 @@ const addJob = async (newJob) => {
     return;
 };
 
+//delete job
 const deleteJob = async (id) => {
     const res = await fetch(`/api/jobs/${id}`, {
         method: "DELETE",
@@ -38,15 +41,24 @@ const deleteJob = async (id) => {
     //console.log("delete", id);
 };
 
+//update job
+const updateJob = async (updateJob) => {
+    const res = await fetch(`/api/jobs/${updateJob.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updateJob),
+    });
+    return;
+};
+
 function App() {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<HomePage />} />
-                <Route
-                    path="/jobs"
-                    element={<JobsPage />}
-                />
+                <Route path="/jobs" element={<JobsPage />} />
                 <Route
                     path="/add-job"
                     element={<AddJobPage addJobSubmit={addJob} />}
@@ -55,6 +67,11 @@ function App() {
                 <Route
                     path="/jobs/:id"
                     element={<JobPage deleteJob={deleteJob} />}
+                    loader={jobLoader}
+                />
+                <Route
+                    path="/edit-job/:id"
+                    element={<EditJobPage updateJobSubmit={updateJob} />}
                     loader={jobLoader}
                 />
             </Route>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "../components/Navbar";
 
 function MainLayout() {
-  return <>
-    <Navbar />
-  <Outlet />
-  </>;
+    return (
+        <>
+            <ToastContainer />
+            <Navbar />
+            <Outlet />
+        </>
+    );
 }
 
 export default MainLayout;
